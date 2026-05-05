9bae46dd86ac6091c7ffdf85eb783578c9a4f1ca
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-05-05 11:05:46 +0100

    moved all constant values to constant file

---
+++ src/core/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://whisperbox.koyeb.app';
+export const WS_URL = 'wss://whisperbox.koyeb.app';
+export const WS_RECONNECT_DELAY_MS = 3000;
---
+++ src/core/store.js
@@ -0,0 +1,23 @@
+export const store = {
+  accessToken: null,
+  refreshToken: null,
+  user: null,
+  privateKey: null,
+  ownPublicKey: null,
+  publicKeyCache: {},
+  onlineUsers: new Set(),
+  ws: null,
+  activeConversation: null,
+
+  clear() {
+    this.accessToken = null;
+    this.refreshToken = null;
+    this.user = null;
+    this.privateKey = null;
+    this.ownPublicKey = null;
+    this.publicKeyCache = {};
+    this.onlineUsers = new Set();
+    this.ws = null;
+    this.activeConversation = null;
+  },
+};
